6de19a02307d64ad0eef1ff213f08a5e3d2cfa02
Vũ Đức Duy <duydvu98@gmail.com>
2023-04-14 13:28:04 +0700

    Improve interface options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { defineInterface } from '@directus/extensions-sdk';
+import { defineInterface, useStores } from '@directus/extensions-sdk';
+import { Field } from '@directus/shared/types';
 import InterfaceComponent from './interface.vue';
 
 export default defineInterface({
@@ -7,35 +8,64 @@ export default defineInterface({
 	icon: 'content_copy',
 	description: 'A presentation interface for displaying a field of a many-to-one field',
 	component: InterfaceComponent,
-	options: [
-		{
-			field: 'm2oField',
-			type: 'string',
-			name: 'M2O Field',
-			meta: {
-				interface: 'input',
-			},
-		},
-		{
-			field: 'presentationField',
-			type: 'string',
-			name: 'Presentation Field',
-			meta: {
-				interface: 'input',
-			},
-		},
-		{
-			field: 'errorMsg',
-			type: 'string',
-			name: 'Error Message',
-			meta: {
-				interface: 'system-input-translated-string',
-				options: {
-					placeholder: 'An error occurred while fetching the related item.',
+	options: (ctx) => {
+		const fieldsStore = useStores().useFieldsStore();
+		const relationsStore = useStores().useRelationsStore();
+
+		const fields = (fieldsStore.getFieldsForCollection(ctx.collection) as Field[])
+			.filter((f) => ['string', 'integer', 'uuid'].includes(f.type))
+			.filter((f) => f.field !== fieldsStore.getPrimaryKeyFieldForCollection(ctx.collection)?.field);
+
+		let m2oFields: Field[] = [];
+		if (ctx.field.meta?.options?.m2oField) {
+			const relation = relationsStore.getRelationForField(ctx.collection, ctx.field.meta?.options?.m2oField);
+			if (relation) {
+				m2oFields = fieldsStore.getFieldsForCollection(relation.related_collection);
+			}
+		}
+
+		return [
+			{
+				field: 'm2oField',
+				type: 'string',
+				name: 'M2O Field',
+				meta: {
+					interface: 'select-dropdown',
+					options: {
+						choices: fields.map((field) => ({
+							text: field.name,
+							value: field.field,
+						})),
+					},
 				},
 			},
-		},
-	],
+			{
+				field: 'presentationField',
+				type: 'string',
+				name: 'Presentation Field',
+				meta: {
+					interface: 'select-dropdown',
+					options: {
+						choices: m2oFields.map((field) => ({
+							text: field.name,
+							value: field.field,
+						})),
+					},
+				},
+			},
+			{
+				field: 'errorMsg',
+				type: 'string',
+				name: 'Error Message',
+				meta: {
+					interface: 'system-input-translated-string',
+					options: {
+						placeholder: 'An error occurred while fetching the related item.',
+					},
+				},
+			},
+		];
+	},
 	types: ['alias'],
 	localTypes: ['presentation'],
 	group: 'presentation',
